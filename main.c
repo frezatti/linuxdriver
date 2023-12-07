21995a792a7f858d45d5ec0d746cce2181a6041d
frezatti <68041159+frezatti@users.noreply.github.com>
2023-12-07 18:56:45 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,26 @@
+#include "device_file.h"
+#include <linux/init.h>       /* module_init, module_exit */
+#include <linux/module.h>     /* version info, MODULE_LICENSE, MODULE_AUTHOR, printk() */
+
+MODULE_DESCRIPTION("Linux driver");
+MODULE_AUTHOR("Adriel Frezatti");
+
+static int simple_driver_init(void)
+{
+    int result = 0;
+    printk( KERN_NOTICE "Linux-driver: Initialization started\n" );
+
+    result = register_device();
+    return result;
+}
+
+static void simple_driver_exit(void)
+{
+    printk( KERN_NOTICE "Linux-driver: Exiting\n" );
+
+    unregister_device();
+}
+
+/*===============================================================================================*/
+module_init(simple_driver_init);
+module_exit(simple_driver_exit);
